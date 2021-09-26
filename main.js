4e1d768b3ae34599dea4f222059e165bb9bf6ebf
Shimanti <676501+Shimanti@users.noreply.github.com>
2021-09-26 00:00:44 -0700

    Updated URL

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener("click", search)
 async function search() {
   let inputItem = document.querySelector('input').value
   try {
-    const res = await fetch(`http://localhost:8000/api/recipes`)
+    const res = await fetch(`https://shrouded-beach-12811.herokuapp.com/api/recipes`)
     const data = await res.json()
     console.log(data)
     data.forEach(o => console.log(o.Title))
